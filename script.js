ee52022c37ade46d091a4c2c4fa5ab1e2a0b57fb
Juan Vargas <jvargas@lacolonial.com.do>
2023-01-11 09:19:11 -0400

    Day 37 Finished.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container'); 
-const pokemon_count = 15;
+const pokemon_count = 150;
 const colors =  {
  fire: '#FDDFDF',
  grass: '#DEFDE0',
@@ -17,6 +17,8 @@ fighting: '#E6E0D4',
 normal: '#F5F5F5'
 };
 
+const main_types = Object.keys(colors);
+
 const fetchPokemons = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i);
@@ -32,20 +34,29 @@ const getPokemon = async (id) => {
 };
 
 const createPokemonCard = (pokemon) => {
+
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon');
 
-    
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id =  pokemon.id.toString().padStart(3, '0');
+
+    //console.log(pokemon.types[0].type.name);
+    const poke_types = pokemon.types.map(type => type.type.name);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1);
+
+    const color = colors[type];
+    pokemonEl.style.backgroundColor = color;
 
     const pokemonInnerHTML = `
-        <div class="pokemon" style="background-color: rgb(222, 253, 224)">
+        <div class="pokemon">
             <div class="img-container">
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
             </div>
             <div class="info">
-                <span class="number">#001</span>
-                <h3 class="name">${pokemon.name}</h3>
-                <small class="type">Type: <span>${pokemon.type}</span></small>
+                <span class="number">#${id}</span>
+                <h3 class="name">${name}</h3>
+                <small class="type">Type: <span>${type}</span></small>
             </div>
         </div>`;
     
