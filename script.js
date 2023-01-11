2fcaad99c2a01bfb6d4d225ae2e2b8c7fc916ad6
Juan Vargas <jvargasc@gmail.com>
2023-01-10 21:04:06 -0400

    Nocturnal Commit.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container'); 
-const pokemon_count = 150;
+const pokemon_count = 15;
 const colors =  {
  fire: '#FDDFDF',
  grass: '#DEFDE0',
@@ -17,7 +17,7 @@ fighting: '#E6E0D4',
 normal: '#F5F5F5'
 };
 
-const fetchPokemons = () => {
+const fetchPokemons = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i);
     }
@@ -27,5 +27,30 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    //console.log(data.id);
+    createPokemonCard(data)
 };
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    
+
+    const pokemonInnerHTML = `
+        <div class="pokemon" style="background-color: rgb(222, 253, 224)">
+            <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+            </div>
+            <div class="info">
+                <span class="number">#001</span>
+                <h3 class="name">${pokemon.name}</h3>
+                <small class="type">Type: <span>${pokemon.type}</span></small>
+            </div>
+        </div>`;
+    
+        pokemonEl.innerHTML = pokemonInnerHTML;
+        poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
